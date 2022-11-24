33e701067dd8f30098f3533c46c58183b33d8225
waltodd <waltodd@hotmail.com>
2022-11-24 14:25:53 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRPeo9XfWd3oBEpzUfKSdSZHUv0F8V2IM",
+    authDomain: "react-chat-63c09.firebaseapp.com",
+    projectId: "react-chat-63c09",
+    storageBucket: "react-chat-63c09.appspot.com",
+    messagingSenderId: "434069803166",
+    appId: "1:434069803166:web:ad26b6e719f657dfd2ab8f"
+  };
